f1ad4891e09933bf8b217fa194555255ae786be1
DevShaded <devshaded@devshaded.com>
2023-07-12 20:46:02 +0200

    ok

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,12 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import vue from '@vitejs/plugin-vue';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.ts'],
-            ssr: 'resources/js/ssr.ts',
+            input: ["resources/css/app.css", "resources/js/app.ts"],
+            ssr: "resources/js/ssr.ts",
             refresh: true,
         }),
         vue({
@@ -19,6 +19,6 @@ export default defineConfig({
         }),
     ],
     ssr: {
-        noExternal: ['@inertiajs/vue3/server'],
+        noExternal: ["@inertiajs/vue3/server"],
     },
 });
