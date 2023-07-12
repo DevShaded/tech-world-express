90d4d1cd15f899ac6918d1e99aa5db3129d3adab
DevShaded <devshaded@devshaded.com>
2023-07-12 20:55:13 +0200

    Add CategoryService CategoryResource & ProductResource

---
+++ app/Http/Resources/Product/ProductResource.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Resources\Product;
+
+use App\Models\Product\Product;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+/** @mixin Product */
+class ProductResource extends JsonResource
+{
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'sku' => $this->sku,
+            'name' => $this->name,
+            'slug' => $this->slug,
+            'information_id' => $this->information_id,
+            'specification_id' => $this->specification_id,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+
+            'information' => $this->whenLoaded('information'),
+            'specification' => $this->whenLoaded('specification'),
+            'faqs' => $this->whenLoaded('faqs'),
+            'reviews' => $this->whenLoaded('reviews'),
+        ];
+    }
+}
---
+++ app/Http/Services/Category/CategoryService.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Services\Category;
+
+use App\Http\Resources\Category\CategoryResource;
+use App\Http\Resources\Product\ProductResource;
+use App\Models\Category\Category;
+use App\Models\Product\Product;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
+use Illuminate\Support\Facades\Cache;
+
+class CategoryService
+{
+    public static function getAllCategories(): AnonymousResourceCollection
+    {
+        $categories = Cache::remember('categories', 3600, function () {
+            return Category::all();
+        });
+
+        return CategoryResource::collection($categories);
+    }
+
+    public static function getCategoryWithProjects(string $name): AnonymousResourceCollection
+    {
+        $products = Cache::remember('products:' . $name, 3600, function () use ($name) {
+            return Product::whereHas('information.category', function ($query) use ($name) {
+                $query->where('name', $name);
+            })->with('information', 'information.productInformationPicture')->get();
+        });
+
+        return ProductResource::collection($products);
+    }
+}
